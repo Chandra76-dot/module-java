1a9904bc622696138c839e2ef28aca61b7365c96
Chandra76-dot <61492749+Chandra76-dot@users.noreply.github.com>
2020-03-12 15:12:29 +0530

    Create FactorialProgram.java

---
+++ FactorialProgram.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class FactorialProgram
+{
+    public static void main(String[] args)
+    {
+        int number = 6;
+        long factorial = 1;
+        
+        Scanner scanner= new Scanner(System.in);
+
+        System.out.println("Enter the Number : ");
+
+        number = scanner.nextInt();
+
+        for (int i = 1; i <= number; i++)
+        {
+            factorial = factorial * i;
+        }
+        System.out.println("Factorial of " + number + " is: " + factorial);
+    }
+}
